338a62370be50053b6e3272caf039130c93cf046
Roboan19 <gonzalezroboan@gmail.com>
2021-07-20 22:48:27 -0500

    RegistrarLogin

--- app/src/main/java/com/dromel/proyectfinaldes6/RegistrarLogin.java
+++ app/src/main/java/com/dromel/proyectfinaldes6/RegistrarLogin.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class IngresarLogin extends AppCompatActivity {
+public class RegistrarLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ingresar_login);
+        setContentView(R.layout.activity_registrar_login);
     }
-
-
 }
